b1704d48eb8c461b090d67d677f1f316bcab61fd
SiddheshAryan <saryan@student.gitam.edu>
2026-03-22 10:49:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -292,6 +292,8 @@ io.on("connection", (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const HOST = "0.0.0.0";
+
+server.listen(PORT, HOST, () => {
+  console.log(`Server running on http://${HOST}:${PORT}`);
 });
